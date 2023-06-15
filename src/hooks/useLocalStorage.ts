6293e50a875fe072978064e5351556ba11401c3e
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-06-15 10:24:17 +0300

    chore: implements useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,19 @@
-const useLocalStorage = () => {
+import { useLayoutEffect, useState } from "react";
 
+
+const useLocalStorage = <T>(name: string) => {
+  const [state, setState] = useState<T>();
+
+  useLayoutEffect(() => {
+    if (window) setState(JSON.parse(localStorage.getItem(name) || "{}"))
+  }, [])
+
+  return {
+    save: (values: T) => {
+      localStorage.setItem(name, JSON.stringify(values || {}))
+      setState(values)
+    }, read: () => state
+  }
 }
 
 export {
